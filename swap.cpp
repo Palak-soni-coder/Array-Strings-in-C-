00cb5a5724ce8ee5c1dff68e157843a9deee1501
Palak_Soni <sonipalak2018@gmail.com>
2025-07-29 15:27:57 +0530

    Create swap.cpp

---
+++ swap.cpp
@@ -0,0 +1,35 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+int arr[6];
+int reverse_array[6];
+cout<<"The numbers of array are:"<<endl;
+
+for(int i=0; i<6; i++){
+    cin>>arr[i];
+}
+
+for(int n=0; n<6; n++){
+    cout<<arr[n]<<" ";
+}
+cout<<"\nReversed array is:";
+for(int i=5; i>=0; i--){
+    cout<<arr[i]<<" ";
+}
+
+   return 0;
+}
+
+
+
+The numbers of array are:
+1
+2
+3
+4
+5
+6
+1 2 3 4 5 6 
+Reversed array is:6 5 4 3 2 1
